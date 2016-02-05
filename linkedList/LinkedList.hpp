0634bb0123c213f47d145a25fcf9f12c420c8d9a
Dustin Wendt <dustinwendt@ku.edu>
2016-02-05 13:41:31 -0600

    Fix search method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -36,13 +36,19 @@ template <typename T>
 bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
-	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
-
-	return(isFound);
+        while(temp != nullptr)
+        {
+            if(temp->getValue() == value)
+            {
+                return(true);
+            }
+            else
+            {
+                temp = temp->getNext();
+            }
+        }
+        return(false);
 }
 
 template <typename T>
